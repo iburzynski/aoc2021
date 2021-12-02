af0d0cdb4eb09968a30e3b301718ae0b8ffcc123
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2021-12-02 01:32:21 -0500

    Refactors Day1 Solution

--- day1/Day1.hs
+++ day1/Day1.hs
@@ -4,22 +4,14 @@ main :: IO ()
 main = do
     content <- readFile "input.txt"
     let depths = map read $ lines content :: [Int]
-    putStrLn $ "Part 1: " ++ show (depthIncreases depths)
+    putStrLn $ "Part 1: " ++ show (windowIncreases 1 depths)
     putStrLn $ "Part 2: " ++ show (windowIncreases 3 depths)
-
-
-depthIncreases :: [Int] -> Int
-depthIncreases = go 0
-    where
-        go acc [] = acc
-        go acc [x] = acc
-        go acc (x:x':xs) = let acc' = if x' > x then acc + 1 else acc
-                           in go acc' (x':xs)
 
 
 windowIncreases :: Int -> [Int] -> Int
 windowIncreases w ds@(_:ds') = go 0 (sumWin ds) ds'
     where
+        go acc _ [] = acc
         go acc prev ds@(_:ds')
             | length ds < w = acc
             | otherwise     = let acc' = if sumWin ds > prev then acc + 1 
